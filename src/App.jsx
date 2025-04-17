f088f496cccd2b35927d2185f28169e9dd04712d
ProfessionalReally <nikita22408@gmail.com>
2025-04-17 19:03:25 +0700

    Implemented button locking, adding values to the list and displaying them on the page

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,19 @@ function App() {
         setError('');
     }
 
+    const onAddButtonClick = () => {
+        if (!error) {
+            setList([
+                ...list,
+                {id: Date.now(), value}
+            ]);
+            setValue('');
+            setError('');
+        }
+    }
+
+    const isValueValid = value.length >= 3;
+
     return (
         <div className={styles.app}>
             <h1 className={styles["page-heading"]}>Ввод значения</h1>
@@ -27,14 +40,26 @@ function App() {
             </p>
             {error && <div className={styles["error"]}>{error}</div>}
             <div className={styles["buttons-container"]}>
-                <button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
-                <button className={styles.button} disabled>Добавить в список</button>
+                <button
+                    className={styles.button}
+                    onClick={onInputButtonClick}
+                >
+                    Ввести новое
+                </button>
+                <button
+                    className={styles.button}
+                    disabled={!isValueValid}
+                    onClick={onAddButtonClick}
+                >
+                    Добавить в список
+                </button>
             </div>
             <div className={styles["list-container"]}>
                 <h2 className={styles["list-heading"]}>Список:</h2>
-                <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
+                {list.length < 1 && <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>}
                 <ul className={styles.list}>
-                    <li className={styles["list-item"]}>Первый элемент</li>
+                    {list.length > 0 && list.map(({id, value}) => (<li className={styles["list-item"]} key={id}>{value}т</li>))
+                    }
                 </ul>
             </div>
         </div>
