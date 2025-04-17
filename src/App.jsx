7ef3f8434ab497df9fd20d95702f47ae92fa6e0c
ProfessionalReally <nikita22408@gmail.com>
2025-04-17 18:16:20 +0700

    cleaned project, added styles and basic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className={styles.app}>
+            <h1 className={styles["page-heading"]}>Ввод значения</h1>
+            <p className={styles["no-margin-text"]}>
+                Текущее значение <code>value</code>: "
+                <output className="current-value"></output>
+                "
+            </p>
+            <div className={styles["error"]}>Введенное значение должно содержать минимум 3 символа</div>
+            <div className={styles["buttons-container"]}>
+                <button className={styles.button}>Ввести новое</button>
+                <button className={styles.button} disabled>Добавить в список</button>
+            </div>
+            <div className={styles["list-container"]}>
+                <h2 className={styles["list-heading"]}>Список:</h2>
+                <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
+                <ul className={styles.list}>
+                    <li className={styles["list-item"]}>Первый элемент</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App
